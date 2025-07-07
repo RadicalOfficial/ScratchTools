237b29546d730624868da7d9688eb3735cbe72c3
Radical <knightbot63905@gmail.com>
2025-07-06 20:06:37 -0400

    Added follow-on-profile. Will make more

---
+++ features/follow-button-on-profile/script.js
@@ -0,0 +1,9 @@
+export default async function ({ feature, console }) {
+    console.log("This is a simple script, I know.")
+
+    // Initiate the HTML collections
+    const followHTML = '<div id="follow-button" class="buttons"><div class="follow-button button notfollowing blue" data-control="follow"><span class="follow text"><span class="icon-sm follower white"></span>Follow</span><span class="unfollow text"><span class="icon-sm follower black"></span>Unfollow</span></div></div>';
+    const profileDesc = document.getElementsByClassName("profile-details");
+
+    profileDesc[0].insertAdjacentHTML(followHTML);
+}
