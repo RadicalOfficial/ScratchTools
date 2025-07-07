62ef2d97da52c327fe5a38e004bd7dcdd5ad7866
Radical <knightbot63905@gmail.com>
2025-07-06 20:38:47 -0400

    Fixed

--- features/follow-button-on-profile/script.js
+++ features/follow-button-on-profile/script.js
@@ -1,9 +1,10 @@
 export default async function ({ feature, console }) {
-    console.log("This is a simple script, I know.")
+  console.log("This is a simple script, I know.");
 
-    // Initiate the HTML collections
-    const followHTML = '<div id="follow-button" class="buttons"><div class="follow-button button notfollowing blue" data-control="follow"><span class="follow text"><span class="icon-sm follower white"></span>Follow</span><span class="unfollow text"><span class="icon-sm follower black"></span>Unfollow</span></div></div>';
-    const profileDesc = document.getElementsByClassName("profile-details");
+  // Initiate the HTML collections
+  const followHTML =
+    '<div id="follow-button" class="buttons"><div class="follow-button button notfollowing blue" data-control="follow"><span class="follow text"><span class="icon-sm follower white"></span>Follow</span><span class="unfollow text"><span class="icon-sm follower black"></span>Unfollow</span></div></div>';
+  const profileDesc = document.getElementsByClassName("profile-details");
 
-    profileDesc[0].insertAdjacentHTML(followHTML);
+  profileDesc[0].insertAdjacentHTML(followHTML);
 }
